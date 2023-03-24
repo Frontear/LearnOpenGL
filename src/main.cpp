ed0aec6a6e960ab3a8f719211fb3d36023a0c650
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-03-24 09:02:05 -0400

    rewrite the main.cpp
    lol, i cant be satisfied, but this should be okay.

--- src/main.cpp
+++ src/main.cpp
@@ -1,183 +1,146 @@
-#ifdef _WIN32
-#define GLFW_DLL
-#endif
-
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 600
-
-#include "shader.h"
-#include "program.h"
+#define GLFW_INCLUDE_NONE // prevents GLFW from linking OpenGL headers, we use GLEW for this
 
 #include <iostream>
 #include <GL/glew.h>
-#include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
-int main(void) {
+int main() {
+    glfwSetErrorCallback([](int error, const char* desc) {
+        std::cerr << "GLFW Error: " << desc << std::endl;
+    });
+
     if (glfwInit() == GLFW_TRUE) {
-        // is OpenGL 4.6 too new?
+        // Use OpenGL version 4.6
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        #ifdef __APPLE__ && __MACH__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-        #endif
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Only allow modern functions, none of the previous immediate mode legacy functions.
 
-        glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE); // for fun
-
-        auto window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+        GLFWwindow* window = glfwCreateWindow(800, 800, "LearnOpenGL", nullptr, nullptr);
         if (window != nullptr) {
             glfwMakeContextCurrent(window);
 
             if (glewInit() == GLEW_OK) {
-                glViewport(0, 0, WIN_WIDTH, WIN_HEIGHT); // sometimes this is set by default when setting the window context. its implementation specific, aka unreliable to expect
-                glfwSetFramebufferSizeCallback(window, [](auto window, int width, int height) {
-                    glViewport(0, 0, width, height);
-                });
+                GLsizei width, height;
+                glfwGetFramebufferSize(window, &width, &height);
+                glViewport(0, 0, width, height);
 
-                frontear::shader vertex_shader;
-                vertex_shader.create(GL_VERTEX_SHADER,
-                R"glsl(
-                #version 460 core
+                const GLchar* vshader = R"glsl(
+                    #version 460 core
 
-                layout (location = 0) in vec2 xy;
-                layout (location = 1) in vec3 rgb;
+                    layout (location = 0) in vec2 xy;
+                    layout (location = 1) in vec3 rgb;
 
-                uniform mat4 matrix = mat4(1.0); // default value of identity matrix
+                    out vec3 v_color;
 
-                out vec4 FragColor;
+                    void main() {
+                        gl_Position = vec4(xy, 1.0, 1.0);
+                        v_color = vec3(rgb);
+                    }
+                )glsl";
 
-                void main() {
-                    gl_Position = matrix * vec4(xy, 1.0, 1.0);
-                    FragColor = vec4(rgb, 1.0);
+                const GLchar* fshader = R"glsl(
+                    #version 460 core
+
+                    in vec3 v_color;
+
+                    out vec4 FragColor;
+
+                    void main() {
+                        FragColor = vec4(v_color, 1.0);
+                    }
+                )glsl";
+
+                GLint status;
+                GLchar report[1024];
+
+                GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+                glShaderSource(vertex_shader, 1, &vshader, nullptr);
+                glCompileShader(vertex_shader);
+
+                glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
+                if (status != GL_TRUE) {
+                    glGetShaderInfoLog(vertex_shader, sizeof(report) / sizeof(GLchar), nullptr, report);
+                    std::cerr << "Error compiling vertex shader:\n" << report << std::endl;
                 }
-                )glsl");
 
-                frontear::shader fragment_shader;
-                fragment_shader.create(GL_FRAGMENT_SHADER,
-                R"glsl(
-                #version 460 core
+                GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+                glShaderSource(fragment_shader, 1, &fshader, nullptr);
+                glCompileShader(fragment_shader);
 
-                in vec4 FragColor;
-                out vec4 diffuseColor; // apparently in GL 4.6 there's no gl_FragColor? idk lol
-
-                void main() {
-                    diffuseColor = FragColor;
+                glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
+                if (status != GL_TRUE) {
+                    glGetShaderInfoLog(fragment_shader, sizeof(report) / sizeof(GLchar), nullptr, report);
+                    std::cerr << "Error compiling fragment shader:\n" << report << std::endl;
                 }
-                )glsl");
 
-                frontear::program shader_program;
-                shader_program.attach(vertex_shader);
-                shader_program.attach(fragment_shader);
-                shader_program.finalize();
+                GLuint shader_program = glCreateProgram();
+                glAttachShader(shader_program, vertex_shader);
+                glAttachShader(shader_program, fragment_shader);
+                glLinkProgram(shader_program);
 
                 GLuint VAO;
                 glGenVertexArrays(1, &VAO);
-                glBindVertexArray(VAO); // stores binded buffers, attrib data
+                glBindVertexArray(VAO);
 
-                int width, height;
-                glfwGetWindowSize(window, &width, &height);
+                GLuint VBO;
+                glGenBuffers(1, &VBO);
+                glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-                float buffer[] = {
-                    // coordinates                                              // colors
-                    -((width / 2.0f) / width),  +((height / 2.0f) / height),    255 / 255.0, 202 / 255.0, 217 / 255.0, // top left
-                    +((width / 2.0f) / width),  +((height / 2.0f) / height),    177 / 255.0, 233 / 255.0, 254 / 255.0, // top right
-                    -((width / 2.0f) / width),  -((height / 2.0f) / height),    255 / 255.0, 202 / 255.0, 217 / 255.0, // bottom left
-                    +((width / 2.0f) / width),  -((height / 2.0f) / height),    177 / 255.0, 233 / 255.0, 254 / 255.0, // bottom right
+                GLuint EBO;
+                glGenBuffers(1, &EBO);
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+                // coordinates are specified in default "front-facing" order, counter clockwise
+                GLfloat buffer_data[] = {
+                    // xy coords    // colors
+                    +0.0f, +0.5f,   1.0f, 1.0f, 1.0f,
+                    -0.5f, -0.5f,   1.0f, 1.0f, 1.0f,
+                    +0.5f, -0.5f,   1.0f, 1.0f, 1.0f,
                 };
 
-                unsigned int index[] = {
-                    0, 1, 2,
-                    1, 2, 3
+                GLuint index_data[] = {
+                    0, 1, 2
                 };
 
-                GLuint BO[1];
-                glGenBuffers(sizeof(BO) / sizeof(GLuint), BO);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(buffer_data), buffer_data, GL_STATIC_DRAW);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_data), index_data, GL_STATIC_DRAW);
 
-                glBindBuffer(GL_ARRAY_BUFFER, BO[0]);
-                glBufferData(GL_ARRAY_BUFFER, sizeof(buffer), buffer, GL_STATIC_DRAW);
-
-                //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, BO[1]);
-                //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
-
-                glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(buffer) / 4, (void*) 0);
-                glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(buffer) / 4, (void*) (2 * sizeof(float)));
+                glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(buffer_data) / 3, (void*) (sizeof(GLfloat) * (&buffer_data[0] - buffer_data)));
+                glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(buffer_data) / 3, (void*) (sizeof(GLfloat) * (&buffer_data[2] - buffer_data)));
                 glEnableVertexAttribArray(0);
                 glEnableVertexAttribArray(1);
 
-                // no need to unbind but usually unbinding the VAO and BO's would go here.
+                glBindVertexArray(0);
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
-                auto radians = 0.0f, x = 0.0f, y = 0.0f;
                 while (glfwWindowShouldClose(window) != GLFW_TRUE) {
-                    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-                        glfwSetWindowShouldClose(window, GLFW_TRUE);
-                    }
-
-                    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-                        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-                            y += 0.05;
-                        }
-                        else {
-                            y += 0.01;
-                        }
-                    }
-                    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-                        if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-                            y -= 0.05;
-                        }
-                        else {
-                            y -= 0.01;
-                        }
-                    }
-                    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-                        if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-                            radians += 0.01;
-                        }
-                        else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-                            x -= 0.05;
-                        }
-                        else {
-                            x -= 0.01;
-                        }
-                    }
-                    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-                        if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
-                            radians -= 0.01;
-                        }
-                        else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-                            x += 0.05;
-                        }
-                        else {
-                            x += 0.01;
-                        }
-                    }
-
-                    glClearColor(0.15, 0.15, 0.15, 0.3);
+                    glClearColor(0.05f, 0.17f, 0.13f, 1.0);
                     glClear(GL_COLOR_BUFFER_BIT);
 
-                    shader_program.use();
-
-                    glm::mat4 matrix(1.0f);
-                    matrix = glm::translate(matrix, glm::vec3(x, y, 0.0));
-                    matrix = glm::rotate(matrix, radians, glm::vec3(0.0, 0.0, 1.0)); // rotate on the z-axis
-
-                    glUniformMatrix4fv(glGetUniformLocation(shader_program.id, "matrix"), 1, GL_FALSE, glm::value_ptr(matrix));
+                    glUseProgram(shader_program);
 
                     glBindVertexArray(VAO);
-                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, index); // if index is set to nullptr, will use EBO buffer data, otherwise can pull directly from here without allocating an EBO.
+                    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
                     glfwSwapBuffers(window);
                     glfwPollEvents();
                 }
+
+                glDeleteShader(vertex_shader);
+                glDeleteShader(fragment_shader);
+                glDeleteProgram(shader_program);
+
+                glDeleteVertexArrays(1, &VAO);
+                glDeleteBuffers(1, &VBO);
+                glDeleteBuffers(1, &EBO);
             }
+
+            glfwDestroyWindow(window);
         }
 
         glfwTerminate();
-
-        return EXIT_SUCCESS;
     }
 
-    return EXIT_FAILURE;
+    return -1;
 }
